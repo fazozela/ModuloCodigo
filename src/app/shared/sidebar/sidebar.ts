39241bdb9e90cd3000c815b146c36dc9a046c0ac
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2025-11-06 00:29:00 -0400

    final version modulo 2

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Sidebar { }
+export class Sidebar {
+  constructor(public authService: AuthService) { }
+}
